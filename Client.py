2cca8aaabcd99496e17dc4e63f4c3d04d142ef82
Kumar Chennupati <70792632+kumarchennupati@users.noreply.github.com>
2021-06-11 13:24:26 +0530

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,49 @@
+import socket
+import cv2
+import pickle
+import struct 
+import threading
+
+
+
+def send(s,serverip):
+    connection = s.makefile('wb')
+    cam = cv2.VideoCapture(0)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    while True:
+        ret, frame = cam.read()
+        result, frame = cv2.imencode('.jpg', frame, encode_param)
+        data = pickle.dumps(frame, 0)
+        size = len(data)
+        s.sendall(struct.pack(">L", size) + data)
+    cam.release()
+
+
+def receive(s,serverip):
+    data = b""
+    payload_size = struct.calcsize(">L")
+    while True:
+        while len(data) < payload_size:
+            data += s.recv(4096)
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += s.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+        cv2.imshow('ImageWindow',frame)
+        cv2.waitKey(1)
+
+
+import socket
+s=socket.socket()
+serverip="192.168.1.7"
+serverport=2222
+s.connect((serverip,serverport))
+t1=threading.Thread(target=receive,args=(s,serverip))
+t2=threading.Thread(target=send,args=(s,serverip))
+t2.start()
+t1.start()
